d463d28afddb3621aff6ab0979281a9b1a03f8da
Hu <1032993648@qq.com>
2018-07-09 19:51:54 +0800

    reset

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import './tools/rem';
 import App from './components/app';
 import './styles/base.less';
 
-
-<<<<<<< HEAD
-
-
-
 fetch(
 	'http://106.12.22.19',
 	{
@@ -20,7 +15,5 @@ fetch(
 	console.log(11111,res,11111111)
 })
 
-=======
->>>>>>> 重置密码初始化
 // Render the main component into the dom
 ReactDOM.render(<Router history={BrowserHistory}><App/></Router>, document.getElementById('app'));
