4800ca3ab7de6827838989b133be5260e368072d
王刚 <564466548@qq.com>
2018-07-09 13:20:21 +0800

    wg

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
+
+
+console.log( fetch )
+fetch(
+	'http://106.12.22.19',
+	{}
+).then( res => {
+	console.log(res)
+})
 // Render the main component into the dom
 ReactDOM.render(<App />, document.getElementById('app'));
