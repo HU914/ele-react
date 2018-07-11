1486e4f4ef4d1d6c0cbe7caac4e45d351338b8a5
hu <1032993648@qq.com>
2018-07-11 18:15:01 +0800

    resvise a cart to reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'; //combineReducers用于组合多个reducer
-import cart from './cart'; //引入cart reducer
+import cart from './reducer'; //引入cart reducer
 
 const all = {
   buyCart:cart
--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,18 +1,7 @@
 import {ADD_TO_CART} from '../actions/actions'; //引入发布的状态
 
-const initialState = {                 //模拟初始数据，
-  cart: [
-    {
-      product: 'bread 700g',
-      quantity: 2,
-      unitCost: 90
-    },
-    {
-      product: 'milk 500ml',
-      quantity: 1,
-      unitCost: 47
-    }
-  ]
+const initialState = {                 //模拟初始状态，
+  cart: []
 }
 
 export default (state=initialState,action) => {     //在初始数据上 发布新数据，先到reducer，检查状态，
